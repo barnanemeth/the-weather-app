6f9a2de4daab6e3371c120eca09ac9e9a03d784d
Németh Barna <barna.nemeth@drukka.hu>
2019-04-15 13:09:09 +0200

    Create initial Xcode project

---
+++ TheWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWeatherApp
+//
+//  Created by Németh Barna on 2019. 04. 15..
+//  Copyright © 2019. Barna Nemeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
