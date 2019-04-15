a2a564ded2707b3bff79c1a8552a9d85aae13125
Németh Barna <barna.nemeth@drukka.hu>
2019-04-15 19:30:34 +0200

    Add City model

---
+++ TheWeatherApp/Models/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  TheWeatherApp
+//
+//  Created by Németh Barna on 2019. 04. 15..
+//  Copyright © 2019. Barna Nemeth. All rights reserved.
+//
+
+import Foundation
+
+struct City: Codable {
+    let name: String
+    let state: String
+}
