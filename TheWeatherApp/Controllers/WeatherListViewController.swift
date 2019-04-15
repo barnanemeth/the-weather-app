62272e326529ec55a252ee2020060078add393c1
Németh Barna <barna.nemeth@drukka.hu>
2019-04-15 19:37:00 +0200

    Load cities in WeatherListViewController

--- TheWeatherApp/Controllers/WeatherListViewController.swift
+++ TheWeatherApp/Controllers/WeatherListViewController.swift
@@ -12,7 +12,11 @@ class WeatherListViewController: UITableViewController {
 
     // MARK: - Properties
     
-    
+    private var cities = [City]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
     
     // MARK: - View life cycle
     
@@ -20,12 +24,23 @@ class WeatherListViewController: UITableViewController {
         super.viewDidLoad()
 
         self.setupNavigationBar()
+        self.loadCities()
     }
     
     // MARK: - Private methods
     
     private func setupNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func loadCities() {
+        guard let citiesResourcePath = Bundle.main.url(forResource: "Cities", withExtension: "json") else { return }
+        guard let citiesData = try? Data(contentsOf: citiesResourcePath) else { return }
+        do {
+            self.cities = try JSONDecoder.init().decode([City].self, from: citiesData)
+        } catch {
+            fatalError("Cities.json resource is malformed")
+        }
     }
 
     // MARK: - Table view data source
