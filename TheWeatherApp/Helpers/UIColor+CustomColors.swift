f3a9b12127c0acaf3b1aaa4b8aec7c2096e1d34a
Németh Barna <barna.nemeth@drukka.hu>
2019-04-15 19:07:48 +0200

    Add UIColor extension for custom colors

---
+++ TheWeatherApp/Helpers/UIColor+CustomColors.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+CustomColors.swift
+//  TheWeatherApp
+//
+//  Created by Németh Barna on 2019. 04. 15..
+//  Copyright © 2019. Barna Nemeth. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var protossPylon: UIColor {
+        return UIColor(red: 0.0, green: 0.66, blue: 1.0, alpha: 1.0)
+    }
+    
+    class var vanadylBlue: UIColor {
+        return UIColor(red: 0.0, green: 0.59, blue: 0.90, alpha: 1.0)
+    }
+    
+}
