1bfc20abb813d1cfc289d0be2ea64dbf6eae0583
Németh Barna <barna.nemeth@drukka.hu>
2019-04-16 15:46:28 +0200

    Add WeatherDetailsViewController class

---
+++ TheWeatherApp/Controllers/WeatherDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherDetailsViewController.swift
+//  TheWeatherApp
+//
+//  Created by Németh Barna on 2019. 04. 16..
+//  Copyright © 2019. Barna Nemeth. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailsViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Private methods
+    
+    
+
+}
