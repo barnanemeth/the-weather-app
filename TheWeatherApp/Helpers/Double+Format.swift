a69b2a347327aa4db128de272aa0c88c5bd38c99
Németh Barna <barna.nemeth@drukka.hu>
2019-04-16 21:32:16 +0200

    Add Double format extension

---
+++ TheWeatherApp/Helpers/Double+Format.swift
@@ -0,0 +1,24 @@
+//
+//  Double+Format.swift
+//  TheWeatherApp
+//
+//  Created by Németh Barna on 2019. 04. 16..
+//  Copyright © 2019. Barna Nemeth. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    
+    var asCelsiusDegree: String {
+        return String(format: "%0.f℃", self)
+    }
+    
+    var asPercent: String {
+        return String(format: "%0.f%", self)
+    }
+    
+    var asPascal: String {
+        return String(format: "%0.fhPa", self)
+    }
+}
