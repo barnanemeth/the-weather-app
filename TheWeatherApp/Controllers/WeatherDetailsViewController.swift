3722c8e2e3526584dcd9f629c29a58196dd57570
Németh Barna <barna.nemeth@drukka.hu>
2019-04-16 21:19:32 +0200

    Setup weather details layout and skeleton view

--- TheWeatherApp/Controllers/WeatherDetailsViewController.swift
+++ TheWeatherApp/Controllers/WeatherDetailsViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import SkeletonView
 
 class WeatherDetailsViewController: UIViewController {
 
     // MARK: - Properties
+    
+    @IBOutlet private weak var cityLabel: UILabel!
+    @IBOutlet private weak var stateLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var minimumTemperatureLabel: UILabel!
+    @IBOutlet private weak var maximumTemperatureLabel: UILabel!
+    @IBOutlet private weak var pressureLabel: UILabel!
+    @IBOutlet private weak var humidityLabel: UILabel!
     
     var city: City?
     
@@ -20,6 +29,7 @@ class WeatherDetailsViewController: UIViewController {
         super.viewDidLoad()
         
         self.setupNavigationBar()
+        self.view.showAnimatedGradientSkeleton()
     }
 
     // MARK: - Private methods
